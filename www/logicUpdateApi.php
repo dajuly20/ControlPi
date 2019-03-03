3fef8f30810eafe4ee35d8b4e37cd347a9c12b3b
Julian Rpi2 <julian@wiche.eu>
2019-03-03 15:18:02 +0200

    Made read work in logicUpdateApi :-) Editor seems to work ... next step: Embed
     Changes to be committed:
    	modified:   www/logicUpdateApi.php
    	new file:   www/webSockCoupler.php

--- www/logicUpdateApi.php
+++ www/logicUpdateApi.php
@@ -2,7 +2,15 @@
 
 if($_SERVER['REQUEST_METHOD'] == "GET"){
 
-$file = file_get_contents("softLoigic.json");
+//$file = file_get_contents("./logic.json",FALSE);
+if($_REQUEST["i"] == "json"){
+$file = file_get_contents('./logic.json', FALSE, NULL);
+}elseif($_REQUEST["i"] == "txt"){
+$file = file_get_contents('./logic.conf', FALSE, NULL);
+
+}else{
+echo "Specify parameter i with json or txt";
+}
 
 echo $file;
 
